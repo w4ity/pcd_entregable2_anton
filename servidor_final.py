7fd20f50e444c5c340fd48ca12fa8f9adaa22aca
w4ity <waityt3stt@gmail.com>
2024-05-23 17:40:47 +0200

    Servidor terminado

---
+++ servidor_final.py
@@ -0,0 +1,37 @@
+from kafka import KafkaProducer
+import json
+import time
+import random
+from datetime import datetime
+import sys
+
+class Producer:
+    def __init__(self, topic):
+        self.topic = topic
+        self.producer = KafkaProducer(
+            bootstrap_servers='localhost:9092',
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
+
+    def generate_temperature_data(self):
+        timestamp = datetime.now().isoformat()
+        temperature = round(random.uniform(15.0, 35.0), 2)  # Genera una temperatura entre 15 y 35 grados
+        return {"timestamp": timestamp, "temperature": temperature}
+
+    def start_producer(self):
+        try:
+            while True:
+                data = self.generate_temperature_data()
+                self.producer.send(self.topic, value=data)
+                print(f"Sent data: {data}")
+                time.sleep(5)
+        except Exception as e:
+            print(f"Error sending data to Kafka: {e}")
+        finally:
+            self.producer.close()
+
+if __name__ == '__main__':
+    # Parámetros para el tópico y la frecuencia
+    topic = 'temp'
+    producer = Producer(topic)
+    producer.start_producer()
